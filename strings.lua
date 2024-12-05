d25e87dd1dc36d20ef77fc32c475717066531511
Florian Thiel <flth@flth.de>
2024-12-05 20:41:25 +0100

    Anpassung Strings

--- strings.lua
+++ strings.lua
@@ -3,29 +3,31 @@ return {
 	en = {
 
 		["mod_name"] = "Pressure gas tank wagon Zagkks",
-		["mod_desc"] = "The Zagkks pressure gas tank wagon is used for transporting liquids and gases and is available in various versions across European railway operations. The version described here has a length of 16.1 meters and was manufactured by companies such as LHB, Brüninghaus, and Ürdingen/WU Siegen, with only minor differences between the variants. Large pressure gas tank wagons have been in use since the 1970s and are characterized by a warning stripe and a manhole at the front. Many of these older designs are still in operation and have been, or continue to be, leased by various gas companies.",
+		["mod_desc"] = "The Zagkks pressure gas tank wagon is used for transporting liquids and gases and is available in various versions across European railway operations. The version described here has a length of 16.1 meters and was manufactured by companies such as LHB, Brüninghaus, and Ürdingen/Waggon Union Siegen, with only minor differences between the variants. Large pressure gas tank wagons have been in use since the 1970s and are characterized by a warning stripe and a manhole at the front. Many of these older designs are still in operation and have been, or continue to be, leased by various gas companies.",
 		["sound_check"] = "Activate custom sound?",
 		["height"] = "Height",
 		["offset x-axis"] = "Offset x-axis",
 		["ground"] = "Ground",
 		["rail"] = "Rail",
 		["vehicle"] = "Vehicle",
-		["zagkks_desc"] = "The Zagkks pressure gas tank wagon is used for transporting liquids and gases and is available in various versions across European railway operations. The version described here has a length of 16.1 meters and was manufactured by companies such as LHB, Brüninghaus, and Ürdingen/WU Siegen, with only minor differences between the variants. Large pressure gas tank wagons have been in use since the 1970s and are characterized by a warning stripe and a manhole at the front. Many of these older designs are still in operation and have been, or continue to be, leased by various gas companies.",
+		["zagkks_desc"] = "The Zagkks pressure gas tank wagon is used for transporting liquids and gases and is available in various versions across European railway operations. The version described here has a length of 16.1 meters and was manufactured by companies such as LHB, Brüninghaus, and Ürdingen/Waggon Union Siegen, with only minor differences between the variants. Large pressure gas tank wagons have been in use since the 1970s and are characterized by a warning stripe and a manhole at the front. Many of these older designs are still in operation and have been, or continue to be, leased by various gas companies.",
 		["schutz"] = "Sun roof",
+        ['menu_Zagkks'] = 'Pressure gas tank wagon Zagkks',
 
 	},
 	de = {
 
 		["mod_name"] = "Druckgaskesselwagen Zagkks",
-		["mod_desc"] = "Der Zagkks-Druckgaskesselwagen wird für den Transport von Flüssigkeiten und Gasen genutzt und ist in verschiedenen Varianten im europäischen Eisenbahnbetrieb verbreitet. Die hier beschriebene Ausführung hat eine Länge von 16,1 Metern und wurde von Herstellern wie LHB, Brüninghaus und Ürdingen/WU Siegen gefertigt. Die Unterschiede zwischen den Varianten verschiedener Hersteller sind gering. Große Druckgaskesselwagen werden seit den 1970er Jahren eingesetzt und sind durch einen Warnstreifen sowie ein Mannloch an der Vorderseite gekennzeichnet. Viele dieser älteren Bauarten sind noch im Einsatz und wurden oder werden von verschiedenen Gasfirmen vermietet.",
+		["mod_desc"] = "Der Zagkks-Druckgaskesselwagen wird für den Transport von Flüssigkeiten und Gasen genutzt und ist in verschiedenen Varianten im europäischen Eisenbahnbetrieb verbreitet. Die hier beschriebene Ausführung hat eine Länge von 16,1 Metern und wurde von Herstellern wie LHB, Brüninghaus und Ürdingen/Waggon Union Siegen gefertigt. Die Unterschiede zwischen den Varianten verschiedener Hersteller sind gering. Große Druckgaskesselwagen werden seit den 1970er Jahren eingesetzt und sind durch einen Warnstreifen sowie ein Mannloch an der Vorderseite gekennzeichnet. Viele dieser älteren Bauarten sind noch im Einsatz und wurden oder werden von verschiedenen Gasfirmen vermietet.",
 		["sound_check"] = "Eigenes Soundset aktivieren?",
 		["height"] = "Höhe",
 		["offset x-axis"] = "Verschub X-Achse",
 		["ground"] = "Boden",
 		["rail"] = "Gleis",
 		["vehicle"] = "Fahrzeug",
-		["zagkks_desc"] = "Der Zagkks-Druckgaskesselwagen wird für den Transport von Flüssigkeiten und Gasen genutzt und ist in verschiedenen Varianten im europäischen Eisenbahnbetrieb verbreitet. Die hier beschriebene Ausführung hat eine Länge von 16,1 Metern und wurde von Herstellern wie LHB, Brüninghaus und Ürdingen/WU Siegen gefertigt. Die Unterschiede zwischen den Varianten verschiedener Hersteller sind gering. Große Druckgaskesselwagen werden seit den 1970er Jahren eingesetzt und sind durch einen Warnstreifen sowie ein Mannloch an der Vorderseite gekennzeichnet. Viele dieser älteren Bauarten sind noch im Einsatz und wurden oder werden von verschiedenen Gasfirmen vermietet.",
+		["zagkks_desc"] = "Der Zagkks-Druckgaskesselwagen wird für den Transport von Flüssigkeiten und Gasen genutzt und ist in verschiedenen Varianten im europäischen Eisenbahnbetrieb verbreitet. Die hier beschriebene Ausführung hat eine Länge von 16,1 Metern und wurde von Herstellern wie LHB, Brüninghaus und Ürdingen/Waggon Union Siegen gefertigt. Die Unterschiede zwischen den Varianten verschiedener Hersteller sind gering. Große Druckgaskesselwagen werden seit den 1970er Jahren eingesetzt und sind durch einen Warnstreifen sowie ein Mannloch an der Vorderseite gekennzeichnet. Viele dieser älteren Bauarten sind noch im Einsatz und wurden oder werden von verschiedenen Gasfirmen vermietet.",
 		["schutz"] = "Sonnenschutz",
+        ['menu_Zagkks'] = 'Druckgaskesselwagen Zagkks',
 
 	},
 }
